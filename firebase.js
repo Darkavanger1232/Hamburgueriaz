85ff3d3071b664bdc64403a4cf39aceec5722a14
Odilon Marcos Franca da Costa <92993415+Darkavanger1232@users.noreply.github.com>
2026-04-08 18:04:01 -0300

    Atualização 08/04

--- firebase.js
+++ firebase.js
@@ -1,15 +1,21 @@
-rules_version = '2';
-service cloud.firestore {
-  match /databases/{database}/documents {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-    match /usuarios/{userId} {
-      allow read, write: if request.auth.uid == userId;
-    }
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBtNslNlc5wm5hg11bp_K4oyMCCG0-pO0w",
+  authDomain: "hamburgueriaz-ede35.firebaseapp.com",
+  projectId: "hamburgueriaz-ede35",
+  storageBucket: "hamburgueriaz-ede35.firebasestorage.app",
+  messagingSenderId: "463524468857",
+  appId: "1:463524468857:web:b1df4f201c812c81cadca8",
+  measurementId: "G-J97QLDK8MV"
+};
 
-    match /pedidos/{id} {
-      allow create: if request.auth != null;
-      allow read: if request.auth.uid == resource.data.userId;
-      allow update: if request.auth != null;
-    }
-  }
-}
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
