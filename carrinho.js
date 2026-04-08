01cc50250b8102e4112bdc92af90a924460f92d0
Odilon Marcos Franca da Costa <92993415+Darkavanger1232@users.noreply.github.com>
2026-04-08 18:23:00 -0300

    Atualização 08/04

---
+++ carrinho.js
@@ -0,0 +1,23 @@
+let carrinho = [];
+
+pagamento.addEventListener("change", () => {
+    pixBox.style.display = pagamento.value === "PIX" ? "block" : "none";
+});
+
+function add(nome, preco){
+    carrinho.push({nome, preco});
+    atualizar();
+}
+
+function atualizar(){
+
+    let total = 0;
+    lista.innerHTML = "";
+
+    carrinho.forEach(i=>{
+        total += i.preco;
+        lista.innerHTML += `<li>${i.nome} - R$ ${i.preco}</li>`;
+    });
+
+    total.innerText = total;
+}
