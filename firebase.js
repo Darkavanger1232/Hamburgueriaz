36104d46523f6cde62ba8c70e20e80e517f85d42
Odilon Marcos Franca da Costa <92993415+Darkavanger1232@users.noreply.github.com>
2026-04-08 15:46:49 -0300

    Atualização 08/04

--- firebase.js
+++ firebase.js
@@ -2,9 +2,6 @@ const firebaseConfig = {
   apiKey: "SUA_KEY",
   authDomain: "SEU_PROJETO.firebaseapp.com",
   projectId: "SEU_ID",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_ID",
-  appId: "SEU_APP_ID"
 };
 
 firebase.initializeApp(firebaseConfig);
