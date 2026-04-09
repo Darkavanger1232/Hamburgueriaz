65f7273b9ceb8add6cdc39b6cbb666e21b2bb506
Odilon Marcos Franca da Costa <92993415+Darkavanger1232@users.noreply.github.com>
2026-04-09 19:43:12 -0300

    Atualização 09/04/2026

--- auth.js
+++ auth.js
@@ -1,42 +1,21 @@
-let modoCadastro = false;
+function login() {
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
 
-function toggleMode(){
-    modoCadastro = !modoCadastro;
-
-    nome.style.display = modoCadastro ? "block" : "none";
-    titulo.innerText = modoCadastro ? "Cadastro" : "Login";
-}
-
-function acaoPrincipal(){
-    modoCadastro ? cadastrar() : login();
-}
-
-function cadastrar(){
-
-    if(!nome.value || !email.value || !senha.value){
-        alert("Preencha tudo");
-        return;
-    }
-
-    auth.createUserWithEmailAndPassword(email.value, senha.value)
-    .then(user => {
-
-        return db.collection("usuarios").doc(user.user.uid).set({
-            nome: nome.value,
-            email: email.value
-        });
-
+    auth.signInWithEmailAndPassword(email, senha)
+    .then(() => {
+        window.location.href = "home.html";
     })
-    .then(()=>{
-        alert("Conta criada!");
-        toggleMode();
-    });
+    .catch(err => alert("Erro: " + err.message));
 }
 
-function login(){
-    auth.signInWithEmailAndPassword(email.value, senha.value)
-    .then(()=>{
-        window.location.href = "inicio.html";
+function cadastrar() {
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+
+    auth.createUserWithEmailAndPassword(email, senha)
+    .then(() => {
+        alert("Conta criada com sucesso!");
     })
-    .catch(()=> alert("Erro login"));
+    .catch(err => alert("Erro: " + err.message));
 }
